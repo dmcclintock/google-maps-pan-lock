c2335a7e7b173f3997c91d567e6f844765ea673f
Daniel McClintock <dmcclintock@acreativecollective.co>
2014-08-29 05:44:00 -0400

    Changed class & id names
    Removed unnecessary comments

--- demo.php
+++ demo.php
@@ -4,47 +4,21 @@
 	<meta charset="UTF-8">
 	<title>Google Maps Pan Lock</title>
 	<link rel="stylesheet" type="text/css" href="includes/styles.css">
-
-	<!-- Begin Google Maps API -->
 	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC3abDeXiFejCuhSme78fem_Ck7UBBk6Fw&sensor=false"></script>
 	<script>
 		google.maps.event.addDomListener( window, 'load', function() {
 			var mapOptions = { zoom: 10, mapTypeId: google.maps.MapTypeId.ROADMAP, center: new google.maps.LatLng(43.020763, -85.741734) }, mapElement = document.getElementById( 'gmpl-map' ), map = new google.maps.Map( mapElement, mapOptions );
 		});
 	</script>
-	<!-- // END Google Maps API -->
-
 </head>
 <body>
-
-	<!--
-
-			============== DIV ID/CLASS NAME CHANGES: ==============
-
-			.map-overlay-lock 								=			.gmpl-btn-lock
-			#mapcontainer 										=			#gmpl-wrapper
-			.map-overlay 											= 		.gmpl-overlay
-
-			NOTE: Commented lines will replace those directly above
-						them once ID/CLASS changes are made.
-	-->
-
-	<a href="#" class="map-overlay-lock"><strong>Lock Map</strong></a>
-	<!-- <a href="#" class="gmpl-btn-lock"><strong>Lock Map</strong></a> -->
-	<!-- ^ TOGGLE ACTIVE, INITIALLY INACTIVE -->
-
-	<div id="mapcontainer" class="map-wrap">
-	<!-- <div id="gmpl-wrapper"> -->
-
-		<a href="#" class="map-overlay active">
-		<!-- <a href="#" class="gmpl-overlay active"> -->
-		<!-- ^ TOGGLE ACTIVE, LOADS ACTIVE -->
+	<a href="#" class="gmpl-btn-lock"><strong>Lock Map</strong></a>
+	<div id="gmpl-wrapper">
+		<a href="#" class="gmpl-overlay active">
 			<strong><span class="touch">Tap </span><span class="screen">Click </span>to pan map</strong>
 		</a>
 		<div id="gmpl-map" style="width: 100%; height: 400px; position: relative; overflow: hidden; -webkit-transform: translateZ(0px); background-color: #F2F2F2;"></div>
-		<!-- <div id="gmpl-map" style="..." -->
 	</div>
-
 	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 	<script type="text/javascript" src="includes/scripts.js"></script>
 </body>
