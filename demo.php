8540c29e37a7580e915748002e9dd85a089b75c4
Daniel McClintock <dmcclintock@acreativecollective.co>
2014-10-31 23:13:55 -0400

    Update demo.php
    There was this small matter of indentation, and it was bothering me...

--- demo.php
+++ demo.php
@@ -14,7 +14,7 @@
 </head>
 <body>
 
-<h1>Google Maps Pan Lock</h1>
+	<h1>Google Maps Pan Lock</h1>
 
 	<p>Lorem ipsum uis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis.</p>
 	<p>Lorem ipsum ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet.</p>
